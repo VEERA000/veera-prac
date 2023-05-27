e80bda8fc4c61969b8d13767c680dddd342da1fb
VEERA000 <veeraveerendra1999@gmail.com>
2023-05-27 10:04:10 +0530

    Added product logic

--- Addition.java
+++ Addition.java
@@ -3,9 +3,8 @@ class Addition
         public static void main(String args[])
         {
                 int num1,num2,res;
-                Scanner s = new Scanner(System.in);
-                System.out.println("Enter num1 value");
                 num1=s.nextInt();
+		System.out.println("This is Prod branch code");
                 System.out.println("Enter num2 value");
                 num2=s.nextInt();
                 res=num1+num2;
